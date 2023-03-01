7b59d26c94a79faa4952c478e0164f776a59ea89
Cosmic <125358161+blue0777@users.noreply.github.com>
2023-03-01 17:55:13 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,7 +1,5 @@
 class guu:
-    api_id = 9567537 
-    api_hash = "316c4522261f8b1ff63ba1b875837d89" 
-    bot_token = "5866542837:AAEkdZZJ86vGPPUNxOtRhzNYuyXi-OZk8Y8" 
-    openai_api_key = "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"
-    model = "text-davinci-003" 
-    mxtoken = 1024
+    api_id = "" 
+    api_hash = "" 
+    bot_token = "" 
+    openai_api_key = ""
